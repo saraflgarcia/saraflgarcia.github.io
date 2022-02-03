5607368d1d4cae5a33672ae6d08923e6eea15ffe
saraflgarcia <49484767+saraflgarcia@users.noreply.github.com>
2022-02-03 17:40:35 +0000

    Add files via upload

---
+++ current.js
@@ -0,0 +1,34 @@
+//Highlight Nav Menu on scroll - through sections
+
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll("nav .menu ul li");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+  sections.forEach( section => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (pageYOffset >= (sectionTop - sectionHeight / 3)) {
+      current = section.getAttribute("id");
+    }
+  })
+
+  navLi.forEach( li => {
+    li.classList.remove("active");
+    if (li.classList.contains(current)) {
+      li.classList.add("active")
+    }
+  })
+})
+
+//Change Nav Menu background color & font color on scroll
+
+const navBar = document.querySelector("nav .menu")
+
+window.onscroll = function() {
+  if (window.pageYOffset > 0) {
+    navBar.classList.add('scrolled')
+  } else {
+    navBar.classList.remove('scrolled')
+  }
+}
